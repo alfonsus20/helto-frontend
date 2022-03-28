15eaf54e2080fb8e18930ecd31157f5fc1378ee9
alfonsus20 <alfonschandrawan@gmail.com>
2022-03-28 13:48:28 +0700

    feat: add card collapse component

---
+++ src/components/CardCollapse/index.tsx
@@ -0,0 +1,48 @@
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import classNames from "classnames";
+import React, { useState } from "react";
+import RadioButton from "../RadioButton";
+import "./style.scss";
+
+const CardCollapse = () => {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+  const [selectedId, setSelectedId] = useState<string>("");
+
+  return (
+    <div className="card-collapse">
+      <div
+        className="card-collapse__title"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        <div>Informasi Wilayah</div>
+        <ChevronDownIcon className="w-5 h-5" />
+      </div>
+      <div
+        className={classNames("card-collapse__body", {
+          "is-collapsed": isCollapsed,
+        })}
+      >
+        <div className="py-2 text-sm">
+          <RadioButton
+            label="Surabaya"
+            name="wilayah"
+            value="surabaya"
+            id="surabaya"
+            onChange={(e) => setSelectedId(e.target.value)}
+          />
+        </div>
+        <div className="py-2 text-sm">
+          <RadioButton
+            label="Jombang"
+            name="wilayah"
+            value="Jombang"
+            id="jombang"
+            onChange={(e) => setSelectedId(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardCollapse;
