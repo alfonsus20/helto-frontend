4cff711f5e3c6f60525c6789f3e2d6dc94ba8771
alfonsus20 <alfonschandrawan@gmail.com>
2022-03-30 01:42:26 +0700

    feat: add scrolltotop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import MobileNavbar from "./components/Navbar/navbar.mobile";
 import { SidebarWrapper } from "./context/SidebarContext";
 import RouteList from "./routes";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <SidebarWrapper>
       <BrowserRouter>
+        <ScrollToTop />
         <main className="min-h-screen flex flex-col">
           <Navbar />
           <MobileNavbar />
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -23,8 +23,6 @@ const Navbar = () => {
         }
       };
 
-      checkViewPort();
-
       window.addEventListener("scroll", checkViewPort);
 
       return () => {
