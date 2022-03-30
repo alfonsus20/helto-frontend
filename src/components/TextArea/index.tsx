b6918b03b91699b825579888c80b1e814d4a8209
alfonsus20 <alfonschandrawan@gmail.com>
2022-03-30 17:59:21 +0700

    feat: add textarea error condition

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -6,17 +6,26 @@ const TextArea = ({
   appearance,
   className,
   rows = 3,
+  isError,
+  helperText,
   ...rest
 }: CommonInputProps & React.TextareaHTMLAttributes<HTMLElement>) => {
   return (
-    <textarea
-      className={classNames(
-        "px-4 py-2 w-full outline-0 focus:outline-1 outline-gray-400 rounded-lg resize-none",
-        className
+    <div className={className}>
+      <textarea
+        className={classNames(
+          "px-4 py-2 w-full outline-none border-2 focus:border-gray-400 rounded-lg resize-none",
+          { "border-red-600 focus:border-red-600": isError }
+        )}
+        rows={rows}
+        {...rest}
+      />
+      {helperText && (
+        <span className={classNames("text-xs", { "text-red-600": isError })}>
+          {helperText}
+        </span>
       )}
-      rows={rows}
-      {...rest}
-    />
+    </div>
   );
 };
 
